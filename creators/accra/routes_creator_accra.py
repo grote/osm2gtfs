9e6a1dee6d4ff2491d40b465bf9294430ceec959
Pascal Rhod <pascal.rhod@canaltp.fr>
2017-08-11 17:32:11 +0200

    Accra - adding frequency and trip-service association

--- creators/accra/routes_creator_accra.py
+++ creators/accra/routes_creator_accra.py
@@ -14,21 +14,21 @@ class RoutesCreatorAccra(RoutesCreator):
         #      "lineas")
 
         # Loop through all lines (master_routes)
-        for line_ref, line in sorted(lines.iteritems()):
-            route = schedule.AddRoute(
-                short_name=line.ref.encode('utf-8'),
-                long_name=line.name,
-                route_type="Bus",
-                route_id=line_ref)
-
-            # AddRoute method add defaut agency as default
-            route.agency_id = schedule.GetDefaultAgency().agency_id
-
-            #route.route_desc = "Esta línea está a prueba"
-
-            route.route_color = "ff0000"
-            route.route_text_color = "ffffff"
-
-            # debug
-            print("información de la linea:", line.name, "agregada.")
+        # for line_ref, line in sorted(lines.iteritems()):
+        #     route = schedule.AddRoute(
+        #         short_name=line.ref.encode('utf-8'),
+        #         long_name=line.name,
+        #         route_type="Bus",
+        #         route_id=line_ref)
+        #
+        #     # AddRoute method add defaut agency as default
+        #     route.agency_id = schedule.GetDefaultAgency().agency_id
+        #
+        #     #route.route_desc = "Esta línea está a prueba"
+        #
+        #     route.route_color = "ff0000"
+        #     route.route_text_color = "ffffff"
+        #
+        #     # debug
+        #     print("información de la linea:", line.name, "agregada.")
         return
