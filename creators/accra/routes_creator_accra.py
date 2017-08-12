527bdc3e926685fb6b287d1813b31de1c6da8e69
Noémie <noemie.lehuby@openmailbox.org>
2017-08-12 18:26:10 +0200

    Accra - add direction_id & headsign on trips

--- creators/accra/routes_creator_accra.py
+++ creators/accra/routes_creator_accra.py
@@ -6,29 +6,8 @@ from creators.routes_creator import RoutesCreator
 class RoutesCreatorAccra(RoutesCreator):
 
     def add_routes_to_schedule(self, schedule, data):
-
         # Get routes information
-        lines = data.get_routes()
-        # debug
-        # print("DEBUG: creando itinerarios a partir de", str(len(lines)),
-        #      "lineas")
+        data.get_routes()
 
-        # Loop through all lines (master_routes)
-        # for line_ref, line in sorted(lines.iteritems()):
-        #     route = schedule.AddRoute(
-        #         short_name=line.ref.encode('utf-8'),
-        #         long_name=line.name,
-        #         route_type="Bus",
-        #         route_id=line_ref)
-        #
-        #     # AddRoute method add defaut agency as default
-        #     route.agency_id = schedule.GetDefaultAgency().agency_id
-        #
-        #     #route.route_desc = "Esta línea está a prueba"
-        #
-        #     route.route_color = "ff0000"
-        #     route.route_text_color = "ffffff"
-        #
-        #     # debug
-        #     print("información de la linea:", line.name, "agregada.")
+        # GTFS routes are created in TripsCreator
         return
