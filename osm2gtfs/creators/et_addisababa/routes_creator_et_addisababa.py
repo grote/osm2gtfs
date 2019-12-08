3d2fb3fb46aa247ebe1d8912e31d43691f038838
Alexander Menk <alex@addismap.com>
2019-12-08 19:38:59 +0100

    First version of Addis Ababa generator
    * no schedule
    * trips copied from gh_accra class

---
+++ osm2gtfs/creators/et_addisababa/routes_creator_et_addisababa.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+import logging
+
+from osm2gtfs.core.elements import Line, Itinerary, Station, Stop
+from osm2gtfs.creators.routes_creator import RoutesCreator
+
+
+class RoutesCreatorEtAddisababa(RoutesCreator):
+    """Empty extend"""
+
---
+++ osm2gtfs/creators/et_addisababa/stops_creator_et_addisababa.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+from osm2gtfs.creators.stops_creator import StopsCreator
+
+
+class StopsCreatorEtAddisababa(StopsCreator):
+
+    # Override construction of stop_id
+    def _define_stop_id(self, stop):
+        # Simply returns osm_id regardless of the osm_type as only map
+        # objects of type nodes are assumed.
+        return str(stop.osm_id)
