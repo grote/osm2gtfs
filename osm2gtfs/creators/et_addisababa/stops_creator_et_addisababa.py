c092176909add22e8260b01391f6e58356dff73d
Alexander Menk <alex@addismap.com>
2023-06-27 22:43:10 +0200

    Always use nodes as ID in Addis Ababa

--- osm2gtfs/creators/et_addisababa/stops_creator_et_addisababa.py
+++ osm2gtfs/creators/et_addisababa/stops_creator_et_addisababa.py
@@ -3,6 +3,11 @@
 from osm2gtfs.creators.stops_creator import StopsCreator
 
 class StopsCreatorEtAddisababa(StopsCreator):
-    """
-    nothing overwritten
-    """
+    def _define_stop_id(self, stop):
+        """
+        We always use the node ID in Addis Ababa because refs currently might contain duplicates
+        """
+
+        stop_id = stop.osm_type + "/" + str(stop.osm_id)
+
+        return stop_id
