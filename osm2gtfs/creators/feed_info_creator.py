bc656f4271a98f1cdb5aa3c634b118487a1e250f
Alexander Menk <alex@addismap.com>
2023-08-08 15:59:39 +0200

    Add contact email and contact URL to Addis Ababa Feed

--- osm2gtfs/creators/feed_info_creator.py
+++ osm2gtfs/creators/feed_info_creator.py
@@ -26,6 +26,8 @@ class FeedInfoCreator(object):
         feed_info = transitfeed.FeedInfo()
         feed_info.feed_publisher_name = config['feed_info']['publisher_name']
         feed_info.feed_publisher_url = config['feed_info']['publisher_url']
+        feed_info.feed_contact_email = config['feed_info']['contact_email']
+        feed_info.feed_contact_url = config['feed_info']['contact_url']
         feed_info.feed_lang = config['agency']['agency_lang']
         feed_info.feed_start_date = config['feed_info']['start_date']
         feed_info.feed_end_date = config['feed_info']['end_date']
